df407628ff4439d2480f1ae54a36aea0303d5ad6
Stefan Krueger <stefan.krueger@crossing.tu-darmstadt.de>
2018-10-16 10:20:31 +0200

    Fixed toString method for logical operators.

--- de.darmstadt.tu.crossing.CryptSL/src-gen/de/darmstadt/tu/crossing/cryptSL/impl/LogicalOperatorImpl.java
+++ de.darmstadt.tu.crossing.CryptSL/src-gen/de/darmstadt/tu/crossing/cryptSL/impl/LogicalOperatorImpl.java
@@ -212,23 +212,18 @@ public class LogicalOperatorImpl extends MinimalEObjectImpl.Container implements
     return super.eIsSet(featureID);
   }
 
-  /**
-   * <!-- begin-user-doc -->
-   * <!-- end-user-doc -->
-   * @generated
-   */
-  @Override
-  public String toString()
-  {
-    if (eIsProxy()) return super.toString();
-
-    StringBuilder result = new StringBuilder(super.toString());
-    result.append(" (OR: ");
-    result.append(or);
-    result.append(", AND: ");
-    result.append(and);
-    result.append(')');
-    return result.toString();
-  }
+	/**
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @generated
+	 */
+	@Override
+	public String toString() {
+		if (or != null && !or.isEmpty()) {
+			return or;
+		} else {
+			return and;
+		}
+	}
 
 } //LogicalOperatorImpl
