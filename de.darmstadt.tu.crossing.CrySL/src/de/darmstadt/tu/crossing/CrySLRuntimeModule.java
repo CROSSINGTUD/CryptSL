1530f334c80b6179a88e554f9d5477e73a449691
Lukas Nagel <lukasngl@mail.upb.de>
2022-10-06 13:31:42 +0200

    add value converter for integers
    Integers may be specified with exponents, i.e. `2^2048`.
    This converter resolves such expressions to a String,
    that is parseable as the Integer result.

--- de.darmstadt.tu.crossing.CrySL/src/de/darmstadt/tu/crossing/CrySLRuntimeModule.java
+++ de.darmstadt.tu.crossing.CrySL/src/de/darmstadt/tu/crossing/CrySLRuntimeModule.java
@@ -4,6 +4,7 @@ import org.eclipse.xtext.linking.impl.ImportedNamesAdapter;
 import org.eclipse.xtext.conversion.IValueConverterService;
 
 import de.darmstadt.tu.crossing.scoping.SilencedImportedNamesAdapter;
+import de.darmstadt.tu.crossing.conversion.CrySLValueConverter;
 
 /**
  * Use this class to register components to be used at runtime / without the Equinox extension registry.
@@ -12,6 +13,10 @@ public class CrySLRuntimeModule extends AbstractCrySLRuntimeModule {
 
 	public Class<? extends ImportedNamesAdapter> bindImportedNamesAdapter() {
 		return SilencedImportedNamesAdapter.class;
+	}
+
+	public Class<? extends IValueConverterService> bindIValueConverterService() {
+		return CrySLValueConverter.class;
 	}
 
 }
