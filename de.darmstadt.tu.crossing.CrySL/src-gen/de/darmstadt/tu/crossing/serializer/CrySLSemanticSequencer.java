8460938a3ea82a96ba932b9fcbd055cdb0714b9f
smeyer198 <s.v.meyer@online.de>
2023-11-06 09:33:55 +0100

    Add deployment for CrySL 2.0.2

--- de.darmstadt.tu.crossing.CrySL/src-gen/de/darmstadt/tu/crossing/serializer/CrySLSemanticSequencer.java
+++ de.darmstadt.tu.crossing.CrySL/src-gen/de/darmstadt/tu/crossing/serializer/CrySLSemanticSequencer.java
@@ -1015,7 +1015,7 @@ public class CrySLSemanticSequencer extends XtypeSemanticSequencer {
 	 *     Primary returns Expression
 	 *
 	 * Constraint:
-	 *     ((orderEv+=[Event|ID] (elementop='+' | elementop='?' | elementop='*')?)? elementop='?'? ((elementop='+' | elementop='*')? elementop='?'?)*)
+	 *     ((orderEv+=[Event|ID] (elementop='+' | elementop='?' | elementop='*')?)? elementop='*'? ((elementop='+' | elementop='?')? elementop='*'?)*)
 	 */
 	protected void sequence_Primary(ISerializationContext context, Expression semanticObject) {
 		genericSequencer.createSequence(context, semanticObject);
