0e4249338bbbaab7e1dc0dd7e42b4b41438630d3
Sven Meyer <s.v.meyer@online.de>
2025-03-18 12:01:03 +0100

    Style

--- CrySLParser/src/main/java/crysl/parsing/CrySLModelReader.java
+++ CrySLParser/src/main/java/crysl/parsing/CrySLModelReader.java
@@ -63,7 +63,6 @@ import java.util.stream.Collectors;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
-import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.xtext.common.types.JvmDeclaredType;
 import org.eclipse.xtext.common.types.JvmTypeReference;
 import org.eclipse.xtext.common.types.access.impl.ClasspathTypeProvider;
